a80b0e8a2151a6a55a968c82e3935e26dcc50724
bluesky5837645 <bluesky5837645@gmail.com>
2022-04-12 10:28:46 +0800

    for the lab0

---
+++ lab0/Debug/FreeRTOS/portable/ARM_CM4F/port.d
@@ -0,0 +1,28 @@
+FreeRTOS/portable/ARM_CM4F/port.o: ../FreeRTOS/portable/ARM_CM4F/port.c \
+ C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/lab0/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/lab0/FreeRTOS/include/FreeRTOSConfig.h \
+ C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/lab0/FreeRTOS/include/projdefs.h \
+ C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/lab0/FreeRTOS/include/portable.h \
+ C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/lab0/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/lab0/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/lab0/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/lab0/FreeRTOS/include/task.h \
+ C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/lab0/FreeRTOS/include/list.h
+
+C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/lab0/FreeRTOS/include/FreeRTOS.h:
+
+C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/lab0/FreeRTOS/include/FreeRTOSConfig.h:
+
+C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/lab0/FreeRTOS/include/projdefs.h:
+
+C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/lab0/FreeRTOS/include/portable.h:
+
+C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/lab0/FreeRTOS/include/deprecated_definitions.h:
+
+C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/lab0/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+
+C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/lab0/FreeRTOS/include/mpu_wrappers.h:
+
+C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/lab0/FreeRTOS/include/task.h:
+
+C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/lab0/FreeRTOS/include/list.h:
