6bbe41732d9c17f2c9ef4612b4c1eb29801d8ccd
bluesky5837645 <bluesky5837645@gmail.com>
2022-05-17 20:12:04 +0800

    lab5

---
+++ lab5/Debug/FreeRTOS/MemMang/heap_4.d
@@ -0,0 +1,28 @@
+FreeRTOS/MemMang/heap_4.o: ../FreeRTOS/MemMang/heap_4.c \
+ C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/fat/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/fat/FreeRTOS/include/FreeRTOSConfig.h \
+ C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/fat/FreeRTOS/include/projdefs.h \
+ C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/fat/FreeRTOS/include/portable.h \
+ C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/fat/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/fat/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/fat/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/fat/FreeRTOS/include/task.h \
+ C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/fat/FreeRTOS/include/list.h
+
+C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/fat/FreeRTOS/include/FreeRTOS.h:
+
+C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/fat/FreeRTOS/include/FreeRTOSConfig.h:
+
+C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/fat/FreeRTOS/include/projdefs.h:
+
+C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/fat/FreeRTOS/include/portable.h:
+
+C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/fat/FreeRTOS/include/deprecated_definitions.h:
+
+C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/fat/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+
+C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/fat/FreeRTOS/include/mpu_wrappers.h:
+
+C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/fat/FreeRTOS/include/task.h:
+
+C:/Users/rong_he/STM32CubeIDE/workspace_1.8.0/fat/FreeRTOS/include/list.h:
